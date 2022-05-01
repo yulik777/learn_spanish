4c7aaa64a0f74376bd697c58319411964386ce54
Yuliia Skabitskaya <yulik777@i.ua>
2022-05-01 13:21:59 +0300

    first App

---
+++ src/components/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './App.css';
+
+
+
+const cards = [
+  {
+    name:"",
+    url:"",
+    text:""
+  },
+  {
+    name:"",
+    url:"",
+    text:""
+  },
+  {
+    name:"",
+    url:"",
+    text:""
+  }]
