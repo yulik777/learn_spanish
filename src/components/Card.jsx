1b95f1ec0c969edcda5945342f7c6946c53bbee7
Yuliia Skabitskaya <yulik777@i.ua>
2022-05-12 14:47:43 +0300

    fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,7 +13,7 @@ function Card (props) {
     <div className="card">
       <div className="card__spanish">{props.spanish}</div>
       <div className="card__translate">{props.translate}</div>
-      <div className="button_show_spanish" onClick = {handleChange}>{pressed ? <div>{props.english}</div> : <button className="button_show">Show</button>}</div>
+      <div className="button_show_spanish" onClick = {handleChange}>{pressed ? <div>{props.english}</div> : <button className="button_show">Translate</button>}</div>
     </div>
   
   );
