a0d957658a69838b0e41714940b18aa9d96a4d4b
Yuliia Skabitskaya <yulik777@i.ua>
2022-06-12 15:00:08 +0300

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './assets/styles/header.scss';
 import logo from './assets/images/logo.png';
 
