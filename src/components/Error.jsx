f65cfd634dad35b3720b86089e84d5a7cbdcdc3b
Yuliia Skabitskaya <yulik777@i.ua>
2022-08-13 12:23:40 +0300

    fixed

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,7 +5,7 @@ import  './assets/styles/error.scss';
 const Error = () => {
     return (
         <div className='error'>
-        <img className='err' src={error} alt="Error"/>
+        <img className='err'  src={error} alt="Error"/>
         </div>
     )
 }
