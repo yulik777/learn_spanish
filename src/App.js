a0f17457e00fb94b7273975bb1ec80671db3a00d
Yuliia Skabitskaya <yulik777@i.ua>
2022-08-17 18:59:42 +0300

    fixed App

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Errorpage from '../src/components/pages/Errorpage.jsx';
 import Homepage from './components/pages/Homepage';
 import Playpage from './components/pages/Playpage';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header  from './components/Header';
+import Footer  from './components/Footer';
 
 
 function AppPages() {
